3227ec141af41072e6d5b1fb796960d50b0e5fae
Nadimul Rahib <nadimulrahib38@gmail.com>
2025-01-26 19:41:52 -0800

    updated

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,11 +3,14 @@ import express, { Application, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { globalErrorHandler } from './app/middlewares/globalErrorHandlers';
 import router from './app/Routes';
+import cookieParser from 'cookie-parser';
+
 const app: Application = express();
 // const port = 3000;
 app.use(express.json());
-app.use(cors());
+app.use(cookieParser());
 
+app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
 app.use('/api', router);
 
 // app.use('/api/auth', authRouter);
