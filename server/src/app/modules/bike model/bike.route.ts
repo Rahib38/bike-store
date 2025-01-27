a510543a9b4d3a0956290315c2fe44f4fb44d145
Nadimul Rahib <nadimulrahib38@gmail.com>
2025-01-26 19:41:14 -0800

    update route

--- server/src/app/modules/bike model/bike.route.ts
+++ server/src/app/modules/bike model/bike.route.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { bikeController } from './bike.controller';
+import { USER_ROLE } from '../auth/auth.interface';
+import auth from '../../middlewares/auth';
 const bikeRoutes = express.Router();
 
-bikeRoutes.post('/', bikeController.createBike);
+bikeRoutes.post('/',auth(USER_ROLE.admin), bikeController.createBike);
 bikeRoutes.get('/:productId', bikeController.singleGetBike);
 bikeRoutes.put('/:productId', bikeController.updateBike);
 bikeRoutes.delete('/:productId', bikeController.deleteBike);
--- server/src/app/modules/order model/order.route.ts
+++ server/src/app/modules/order model/order.route.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import { orderController } from './order.controller';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from '../auth/auth.interface';
 const orderRoutes = express.Router();
 
-orderRoutes.post('/', orderController.createOrder);
-orderRoutes.get('/revenue', orderController.getTotalRevenue);
+orderRoutes.post('/',auth(USER_ROLE.admin), orderController.createOrder);
+orderRoutes.get('/revenue',auth(USER_ROLE.customer), orderController.getTotalRevenue);
 export  default orderRoutes
