da5788fa456e825e169f79da8af1b1bae3b4bbf1
Nadimul Rahib <nadimulrahib38@gmail.com>
2025-01-27 20:37:51 -0800

    added some page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -3,8 +3,40 @@ import { FcGoogle } from "react-icons/fc";
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
+import { useLoginMutation } from "@/Redux/Features/Auth/AuthApi";
+import { setUser } from "@/Redux/Features/Auth/AuthSlice";
+import { verifyToken } from "@/utils/verifyToken";
+import { FieldValues, useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+import { useAppDispatch } from "@/Redux/hooks";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [login] = useLoginMutation();
+  const dispatch = useAppDispatch();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = async (data: FieldValues) => {
+    const toastId = toast.loading("Logging in");
+    try {
+      const userInfo = {
+        email: data?.email,
+        password: data.password,
+      };
+      const res = await login(userInfo).unwrap();
+      const user = verifyToken(res.data.token);
+      dispatch(setUser({ user: user, token: res?.data.token }));
+      toast.success('Logged in',{id:toastId,duration:2000})
+      navigate('/')
+    } catch  {
+      toast.error('Something went wrong', { id: toastId, duration: 2000 });
+    }
+  };
   return (
     <section className="py-32 max-h-full">
       <div className="container mx-auto ">
@@ -24,14 +56,43 @@ const Login = () => {
               </p>
             </div>
             <div>
-              <div className="grid gap-4">
-                <Input type="email" placeholder="Enter your email" required />
+              <form onSubmit={handleSubmit(onSubmit)} className="grid gap-4">
+                <div>
+                  <Input
+                    type="email"
+                    placeholder="Enter your email"
+                    required
+                    {...register("email", {
+                      required: "Email is required",
+                      pattern: {
+                        value: /^\S+@\S+$/i,
+                        message: "Invalid email address",
+                      },
+                    })}
+                  />
+                  {errors.email && (
+                    <p className="text-red-500 text-sm mt-1">
+                      {errors.email.message}
+                    </p>
+                  )}
+                </div>
                 <div>
                   <Input
                     type="password"
                     placeholder="Enter your password"
-                    required
+                    {...register("password", {
+                      required: "Password is required",
+                      minLength: {
+                        value: 6,
+                        message: "Password must be at least 6 characters",
+                      },
+                    })}
                   />
+                  {errors.password && (
+                    <p className="text-red-500 text-sm mt-1">
+                      {errors.password.message}
+                    </p>
+                  )}
                 </div>
                 <div className="flex justify-between">
                   <div className="flex items-center space-x-2">
@@ -51,23 +112,23 @@ const Login = () => {
                   </a>
                 </div>
                 <Button type="submit" className="mt-2 w-full">
-                  Create an account
+                  Sign in account
                 </Button>
                 <Button variant="outline" className="w-full">
                   <FcGoogle className="mr-2 size-5" />
                   Sign up with Google
                 </Button>
-              </div>
+              </form>
               <div className="mx-auto mt-8 flex justify-center gap-1 text-sm text-muted-foreground">
                 <p>Don&apos;t have an account?</p>
-                <a href="#" className="text-primary hover:underline">
-                  Sign up
+                <a className="text-primary hover:underline">
+                  <Link to="/register"> Sign up</Link>
                 </a>
               </div>
             </div>
           </div>
         </div>
-{/* 
+        {/* 
         <div>
           <img
             src="https://library.shadcnblocks.com/images/block/placeholder-1.svg"
--- client/src/route/rotuer.tsx
+++ client/src/route/rotuer.tsx
@@ -1,10 +1,37 @@
 import App from "@/App";
+import About from "@/pages/About";
+import AllProductsPage from "@/pages/AllProductsPage";
+import Home from "@/pages/Home";
+import Login from "@/pages/Login";
+import Register from "@/pages/Register";
 import { createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children:[
+      {
+        path:"/",
+        element:<Home></Home>
+      },
+      {
+        path:"/allProduct",
+        element:<AllProductsPage></AllProductsPage>
+      },
+      {
+        path:"/about",
+        element:<About></About>
+      },
+    ]
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
   },
 ]);
 
