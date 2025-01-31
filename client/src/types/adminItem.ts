97ff235b0651444a005b2510d4bd1b2b19096747
Nadimul Rahib <nadimulrahib38@gmail.com>
2025-01-30 22:25:50 -0800

    added

---
+++ client/src/types/adminItem.ts
@@ -0,0 +1,13 @@
+export type TAdminProduct = {
+  _id: string;
+  name: string;
+  brand: string;
+  price: number;
+  category: string;
+  description: string;
+  quantity: number;
+  inStock: boolean;
+  createdAt: string;
+  updateAt: string;
+  __v: number;
+};
