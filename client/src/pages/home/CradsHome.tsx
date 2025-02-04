4e0f335e078f57ff9ae463d2c5456d56ee2bde74
Nadimul Rahib <nadimulrahib38@gmail.com>
2025-02-03 23:34:18 -0800

    added

--- client/src/pages/home/CradsHome.tsx
+++ client/src/pages/home/CradsHome.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -27,33 +28,49 @@ const CardsHome = () => {
   );
   console.log(productData);
   return (
-    <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-5 container mx-auto mt-8">
-      {productData?.map((item) => (
-        <Card>
-          <img src={item.image} alt="" />
-          <CardHeader>
-            <CardTitle>{item?.name}</CardTitle>
+    <div className="mt-5">
+         <div className="flex justify-center items-center mt-5">
+      
+          <h2 className="text-3xl font-semibold">
+         Lastest Product
+          </h2>
+    
+      </div>
+      <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-5 container mx-auto mt-8">
+        {productData?.map((item) => (
+          <Card>
+            <img src={item.image} alt="" />
+            <CardHeader>
+              <CardTitle>{item?.name}</CardTitle>
 
-            <CardDescription className="font-bold">
-            
-              <span className=" font-semibold text-gray-800 mb-4">
-                Brand:
-              </span>
-              {item?.brand}
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>price: ${item?.price}</p>
-          </CardContent>
-          <CardFooter>
-            <Link to={`/cardDetails/${item?._id}`}>
-              <button className="bg-emerald-400 text-white  rounded-xl p-2 px-4 ">
-                View Details
-              </button>
-            </Link>
-          </CardFooter>
-        </Card>
-      ))}
+              <CardDescription className="font-bold">
+                <span className=" font-semibold text-gray-800 mb-4">
+                  Brand:
+                </span>
+                {item?.brand}
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>price: ${item?.price}</p>
+            </CardContent>
+            <CardFooter>
+              <Link to={`/cardDetails/${item?._id}`}>
+                <button className="bg-emerald-400 text-white  rounded-xl p-2 px-4 ">
+                  View Details
+                </button>
+              </Link>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+      <div className="flex justify-center items-center mt-5">
+        <Link to={"/allProduct"}>
+          {" "}
+          <Button className="bg-emerald-400 hover:bg-emerald-400">
+            View All
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 };
