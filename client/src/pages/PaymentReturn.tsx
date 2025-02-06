a08b7b3f5341cde5d88122f6aa6ec65619101412
Nadimul Rahib <nadimulrahib38@gmail.com>
2025-02-06 02:11:54 -0800

    update

--- client/src/pages/PaymentReturn.tsx
+++ client/src/pages/PaymentReturn.tsx
@@ -1,14 +1,34 @@
-import { Link } from 'react-router-dom'
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Link, useSearchParams } from "react-router-dom";
 
 const PaymentReturn = () => {
+  const [searchParams] = useSearchParams();
+  const invoice = searchParams.get("order_id");
   return (
     <div>
-      <Link to={"/userDashboard/myOrder"}>
-      
-      <h1>Go to order</h1>
-      </Link>
-    </div>
-  )
-}
+      <div className="h-screen flex items-center justify-center bg-gray-100 text-gray-800">
+        <Card className="w-96 shadow-lg">
+          <CardHeader>
+            <CardTitle className="text-4xl font-bold text-center">
+              404
+            </CardTitle>
+          </CardHeader>
+          <CardContent className="text-center">
+            <h2 className="text-xl font-semibold mb-2">Order Create</h2>
+            <p className="text-gray-600 mb-6">Your transaction ID is:</p>
+            <div className="bg-gray-100 text-black py-3 px-6 rounded-lg font-mono text-lg shadow-inner mb-6">
+              {invoice || "N/A"}
+            </div>
 
-export default PaymentReturn
+            <Button asChild>
+              <Link to={"/userDashboard/myOrder"}>My Orders</Link>
+            </Button>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export default PaymentReturn;
