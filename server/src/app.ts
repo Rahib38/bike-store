dd370df4acba584170366faa14a7204f34a24a9d
Nadimul Rahib <nadimulrahib38@gmail.com>
2025-12-28 23:39:55 +0600

    deploy update

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,7 +10,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({ origin: ['http://localhost:5173',"https://bike-store-liart-seven.vercel.app","*"], credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 app.use('/api', router);
 
 // app.use('/api/auth', authRouter);
